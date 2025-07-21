11ee5807368b1d6335a6f6f5167b39c8389a66fe
kunal sharma <kunalpisj@gmail.com>
2025-07-21 18:45:27 +0530

    add tasks and remove tasks

--- taskmanager/src/main/java/com/scalar/taskmanager/HelloController.java
+++ taskmanager/src/main/java/com/scalar/taskmanager/HelloController.java
@@ -1,4 +1,13 @@
 package com.scalar.taskmanager;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    String hello() {
+        return "Hello World!";
+    }
 }
--- taskmanager/src/main/java/com/scalar/taskmanager/controllers/TaskController.java
+++ taskmanager/src/main/java/com/scalar/taskmanager/controllers/TaskController.java
@@ -1,4 +1,41 @@
 package com.scalar.taskmanager.controllers;
 
+import com.scalar.taskmanager.dto.CreateTaskDTO;
+import com.scalar.taskmanager.entities.TaskEntity;
+import com.scalar.taskmanager.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    public final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<TaskEntity>> getTasks() {
+        var tasks = taskService.getTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskEntity> getTasksById(@PathVariable("id") Integer id) {
+        var task = taskService.getTaskById(id);
+        if(task == null) {
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(task);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) {
+        var task = taskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
+        return ResponseEntity.ok(task);
+    }
+
 }
